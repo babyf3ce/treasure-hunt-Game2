8f55f0e15ddeb634dbff75ae428be3e895377224
babyf3ce <73721485+babyf3ce@users.noreply.github.com>
2021-06-26 13:15:21 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("L R A A B V I E ", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("B D A A E A T S", 700,120)
+    fill("lightblue")
+    text("Hint: Holds all your online code !", 700,160)
+
+    fill("white")
+    textSize(15)
+    text("N C F I T U N I O", 100,380)
+    fill("lightblue")
+    text("Hint: Performs a particular task !", 100,400)
+} 
